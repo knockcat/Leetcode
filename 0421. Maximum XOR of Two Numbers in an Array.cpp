8465f3c254ea9ab825212955df73ca52176aa525
VICKY <vishujoshi21nov2000@gmail.com>
2022-01-27 23:19:22 +0530

    Solution as on 27-01-2022 11:13 pm

--- 0421. Maximum XOR of Two Numbers in an Array.cpp
+++ 0421. Maximum XOR of Two Numbers in an Array.cpp
@@ -7,6 +7,37 @@
 //  0 1  1
 //  1 0  1
 
+class Solution
+{
+public:
+    int findMaximumXOR(vector<int> &nums)
+    {
+        int ans = 0, prefix = 0;
+
+        for (int i = 31; i >= 0; i--)
+        {
+            prefix = prefix | (1 << i);
+
+            unordered_set<int> s;
+
+            for (int n : nums)
+                s.insert(n & prefix);
+
+            int c = ans | (1 << i);
+            for (int a : s)
+            {
+                int b = c ^ a;
+                if (s.find(b) != s.end()) // if b exists
+                {
+                    ans = c;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
 /*      TLE Solution
 
 class Solution {
