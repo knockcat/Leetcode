1d5767f42ca951a00523c5aa5b598a56facdcb26
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-01 10:33:55 +0530

    Solution as on 01-02-2022 10:33 am

---
+++ 0258. Add Digits.cpp
@@ -0,0 +1,12 @@
+// 258.✅ Add Digits
+
+class Solution
+{
+public:
+    int addDigits(int num)
+    {
+        if (num % 9 == 0 && num != 0)
+            return 9;
+        return (num % 9);
+    }
+};
