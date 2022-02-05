e8f693e8bd0ff7547d1f88e8806199a126a10520
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-05 20:33:29 +0530

    Solution as on 05-02-2022 08:33 pm

---
+++ 0119. Pascal's Triangle II.cpp
@@ -0,0 +1,17 @@
+// 119.✅ Pascal's Triangle II
+
+class Solution
+{
+public:
+    vector<int> getRow(int rowIndex)
+    {
+        vector<int> res(rowIndex + 1, 1);
+
+        for (int i = 0; i < rowIndex; ++i)
+        {
+            for (int j = i; j > 0; --j)
+                res[j] += res[j - 1];
+        }
+        return res;
+    }
+};
