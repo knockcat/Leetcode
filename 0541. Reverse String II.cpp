4dbb1eaf6f373073f34cae26b1cd79a4f743c747
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-07 08:10:12 +0530

    Solution as on 07-02-2022 08:10 am

---
+++ 0541. Reverse String II.cpp
@@ -0,0 +1,17 @@
+// 541.✅ Reverse String II
+
+class Solution
+{
+public:
+    string reverseStr(string s, int k)
+    {
+        for (int i = 0; i < s.size(); i += 2 * k)
+        {
+            if (i + k < s.size())
+                reverse(s.begin() + i, s.begin() + i + k);
+            else
+                reverse(s.begin() + i, s.end());
+        }
+        return s;
+    }
+};
