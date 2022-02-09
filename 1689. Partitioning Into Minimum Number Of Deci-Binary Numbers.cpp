f2c9a071c55cf166a48ca74f096d52fe5b151d68
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-09 08:55:39 +0530

    Solution as on 09-02-2022 08:55 am

---
+++ 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -0,0 +1,8 @@
+// 1689.✅ Partitioning Into Minimum Number Of Deci-Binary Numbers
+
+class Solution {
+public:
+    int minPartitions(string n) {
+        return *max_element(n.begin(),n.end()) - '0';
+    }
+};
