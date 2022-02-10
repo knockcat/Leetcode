c4606a9bee03940791c4c1ff9d9b2221597cb3a1
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-10 19:19:08 +0530

    Solution as on 10-02-2022 07:19 pm

---
+++ 1313. Decompress Run-Length Encoded List.cpp
@@ -0,0 +1,16 @@
+// 1313.✅ Decompress Run-Length Encoded List
+
+class Solution
+{
+public:
+    vector<int> decompressRLElist(vector<int> &nums)
+    {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i += 2)
+        {
+            for (int j = 0; j < nums[i]; ++j)
+                ans.push_back(nums[i + 1]);
+        }
+        return ans;
+    }
+};
