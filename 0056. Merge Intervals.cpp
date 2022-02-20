3a34607f493f559ae917de0117d80186de69926c
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-20 19:21:27 +0530

    Solution as on 20-02-2022 07:21 pm

---
+++ 0056. Merge Intervals.cpp
@@ -0,0 +1,33 @@
+// 56.✅ Merge Intervals
+
+class Solution
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+
+        if (intervals.size() == 0)
+            return ans;
+
+        sort(intervals.begin(), intervals.end());
+        ans.push_back(intervals[0]);
+
+        int j = 0;
+
+        for (int i = 1; i < intervals.size(); ++i)
+        {
+            if (ans[j][1] >= intervals[i][0])
+                ans[j][1] = max(ans[j][1], intervals[i][1]);
+
+            else
+            {
+                ++j;
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
