a0ca0206f52e25f6a7ff7b5c2402c4b073244430
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-23 07:15:08 +0530

    Solution as on 22-02-2022 10:08 pm

--- 0199. Binary Tree Right Side View.cpp
+++ 0199. Binary Tree Right Side View.cpp
@@ -1,40 +1,40 @@
 // 199.✅ Binary Tree Right Side View
 
-class Solution {
+class Solution
+{
 public:
     vector<int> ans;
-    
-    void Right(TreeNode* root)
+
+    void Right(TreeNode *root)
     {
-        if(root == NULL)
+        if (root == NULL)
             return;
-        queue<TreeNode*> q;
+        queue<TreeNode *> q;
         q.push(root);
-        
-        while(!q.empty())
+
+        while (!q.empty())
         {
-            int n = q.size();    
-            for(int i = 0; i<n; ++i)
+            int n = q.size();
+            for (int i = 0; i < n; ++i)
             {
                 TreeNode *curr = q.front();
                 q.pop();
-                
-                if(i == n-1)
+
+                if (i == n - 1)
                     ans.push_back(curr->val);
-    
-                if(curr->left != NULL)
+
+                if (curr->left != NULL)
                     q.push(curr->left);
-                if(curr->right != NULL)
+                if (curr->right != NULL)
                     q.push(curr->right);
             }
-            
         }
     }
-    vector<int> rightSideView(TreeNode* root) {
-        
-       Right(root);
-        
+    vector<int> rightSideView(TreeNode *root)
+    {
+
+        Right(root);
+
         return ans;
-        
     }
 };
