590fa40d6190b78cb6409c5d2f6924f6e810beca
VICKY <vishujoshi21nov2000@gmail.com>
2022-03-23 19:40:03 +0530

    Solution as on 23-03-2022 07:17 pm

--- 2139. Minimum Moves to Reach Target Score.cpp
+++ 2139. Minimum Moves to Reach Target Score.cpp
@@ -23,3 +23,47 @@ public:
         return res + target - 1;
     }
 };
+
+//recursive
+class Solution
+{
+public:
+    int CountMoves(int target, int maxDoubles)
+    {
+        if (target == 1)
+            return 0;
+        if (maxDoubles == 0)
+            return target - 1;
+        if (target & 1)
+            return 1 + CountMoves(target - 1, maxDoubles);
+        return 1 + CountMoves(target / 2, maxDoubles - 1);
+    }
+
+    int minMoves(int target, int maxDoubles)
+    {
+
+        return CountMoves(target, maxDoubles);
+    }
+};
+
+// recusive using cnt
+class Solution
+{
+public:
+    int CountMoves(int target, int maxDoubles, int cnt)
+    {
+        if (target == 1)
+            return cnt;
+        if (maxDoubles == 0)
+            return target - 1;
+        if (target & 1)
+            return cnt = 1 + CountMoves(target - 1, maxDoubles, cnt);
+        return cnt = 1 + CountMoves(target / 2, maxDoubles - 1, cnt);
+    }
+
+    int minMoves(int target, int maxDoubles)
+    {
+
+        return CountMoves(target, maxDoubles, 0);
+    }
+};
