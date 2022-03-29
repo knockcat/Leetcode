acf063b9ebb87f7c1173a0092cf5495a94ebeba9
VICKY <vishujoshi21nov2000@gmail.com>
2022-03-29 22:03:45 +0530

    Solution as on 29-03-2022 6:00 am

--- 0287. Find the Duplicate Number.cpp
+++ 0287. Find the Duplicate Number.cpp
@@ -64,3 +64,52 @@ public:
         return ind;
     }
 };
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int low = 1, high = nums.size() - 1, cnt;
+        
+        while(low <=  high)
+        {
+            int mid = low + (high - low) / 2;
+            cnt = 0;
+            // cnt number less than equal to mid
+            for(int n : nums)
+            {
+                if(n <= mid)
+                    ++cnt;
+            }
+            // binary search on left
+            if(cnt <= mid)
+                low = mid + 1;
+            else
+            // binary search on right
+                high = mid - 1;
+            
+        }
+        return low;
+    }
+	// for github repository link go to my profile.
+};
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int, int> m;
+        int duplicate = 0;
+        for(auto i= 0; i<nums.size(); ++i)
+        ++m[nums[i]];
+        
+        for(auto i : m)
+        {
+            if(i.second > 1)
+            {
+                duplicate = i.first;
+                break;
+            }
+        }
+        return duplicate;
+    }
+	// for github repository link go to my profile.
+};
