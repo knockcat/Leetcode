a8c8fc512017f7a18493f655cd025c39301e76e5
VICKY <vishujoshi21nov2000@gmail.com>
2022-04-01 17:32:49 +0530

    Solution as on 01-04-2022 05:32 pm

--- 0344. Reverse String.cpp
+++ 0344. Reverse String.cpp
@@ -8,3 +8,49 @@ public:
         reverse(s.begin(), s.end());
     }
 };
+
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        stack<char> st;
+
+        for (auto i : s)
+            st.push(i);
+        s.clear();
+        while (!st.empty())
+        {
+            s.push_back(st.top());
+            st.pop();
+        }
+    }
+};
+
+class Solution
+{
+public:
+    void solve(vector<char> &s, int i, int j)
+    {
+        if (i > j)
+            return;
+        swap(s[i], s[j]);
+        solve(s, ++i, --j);
+    }
+
+    void reverseString(vector<char> &s)
+    {
+        solve(s, 0, s.size() - 1);
+    }
+};
+
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        int i = 0, j = s.size() - 1;
+        while (i < j)
+            swap(s[i++], s[j--]);
+    }
+};
