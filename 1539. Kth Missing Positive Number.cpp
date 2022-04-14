c0fb7876533b6f6b7c89877e97ca03634a237afc
VICKY <vishujoshi21nov2000@gmail.com>
2022-04-14 06:57:58 +0530

    Solution as on 14-04-2022 07:00 am

---
+++ 1539. Kth Missing Positive Number.cpp
@@ -0,0 +1,42 @@
+1539. Kth Missing Positive Number
+
+    class Solution
+{
+public:
+    int findKthPositive(vector<int> &arr, int k)
+    {
+        if (arr[0] > k)
+            return k;
+
+        int num = k;
+
+        for (int i : arr)
+        {
+            if (i <= num)
+            {
+                num++;
+            }
+            else
+                break;
+        }
+        return num;
+    }
+};
+
+class Solution
+{
+public:
+    int findKthPositive(vector<int> &arr, int k)
+    {
+        int start = 0, end = arr.size();
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] - mid - 1 < k)
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        return start + k;
+    }
+};
