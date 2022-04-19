b0c48cf61e60c4ce0a850eb7fe0f54175043d347
VICKY <vishujoshi21nov2000@gmail.com>
2022-04-19 19:33:55 +0530

    Solution as on 19-04-2022 07:37 pm

--- 0167. Two Sum II - Input Array Is Sorted.cpp
+++ 0167. Two Sum II - Input Array Is Sorted.cpp
@@ -1,26 +1,51 @@
 // 167. Two Sum II - Input Array Is Sorted
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        vector <int> v;
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        vector<int> v;
         int l = 0, r = numbers.size() - 1;
-        while(l < r)
+        while (l < r)
         {
-            if(numbers[l] + numbers[r] == target)
+            if (numbers[l] + numbers[r] == target)
             {
-                v.push_back(l+1);
-                v.push_back(r+1);
+                v.push_back(l + 1);
+                v.push_back(r + 1);
                 break;
             }
-            else if(numbers[l] + numbers[r] < target){
+            else if (numbers[l] + numbers[r] < target)
+            {
                 l++;
-                
             }
-            else 
+            else
                 r--;
-            
         }
         return v;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        for (int i = 0; i < numbers.size() - 1; ++i)
+        {
+            int start = i + 1, end = numbers.size();
+            while (start < end)
+            {
+                int t = target - numbers[i];
+                int mid = start + (end - start) / 2;
+                if (numbers[mid] == t)
+                    return {i + 1, mid + 1};
+                else if (numbers[mid] >= t)
+                    end = mid;
+                else
+                    start = mid + 1;
+            }
+        }
+        return {};
     }
 };
