bf98e3ac3b245a24863c5a343c3b14b72c64b049
VICKY <vishujoshi21nov2000@gmail.com>
2022-04-26 09:51:39 +0530

    Solution as on 26-04-2022 09:50 am

---
+++ 0284. Peeking Iterator.cpp
@@ -0,0 +1,30 @@
+// 284.✅ Peeking Iterator
+
+class PeekingIterator : public Iterator
+{
+public:
+    PeekingIterator(const vector<int> &nums) : Iterator(nums)
+    {
+        // Initialize any member here.
+        // **DO NOT** save a copy of nums and manipulate it directly.
+        // You should only use the Iterator interface methods.
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    int peek()
+    {
+        return Iterator(*this).next();
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    int next()
+    {
+        return Iterator::next();
+    }
+
+    bool hasNext() const
+    {
+        return Iterator::hasNext();
+    }
+};
