09fcadbd7a041a7ae868b4291c33e9bb966402a8
VICKY <vishujoshi21nov2000@gmail.com>
2022-04-29 07:07:24 +0530

    Solution as on 29-04-2022 07:25 am

---
+++ 0535. Encode and Decode TinyURL.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl)
+    {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl)
+    {
+        return shortUrl;
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
