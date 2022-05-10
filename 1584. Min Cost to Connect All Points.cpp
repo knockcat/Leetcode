0cf626a23c0a7916c1f34e6f09e8e1568c449325
VICKY <vishujoshi21nov2000@gmail.com>
2022-05-10 23:13:00 +0530

    Solution as on 10-05-2022 11:12 pm

---
+++ 1584. Min Cost to Connect All Points.cpp
@@ -0,0 +1,25 @@
+// 1584.✅ Min Cost to Connect All Points
+
+class Solution
+{
+public:
+    int minCostConnectPoints(vector<vector<int>> &ps)
+    {
+        int n = ps.size(), res = 0, i = 0, connected = 0;
+        vector<bool> visited(n);
+        priority_queue<pair<int, int>> pq;
+        while (++connected < n)
+        {
+            visited[i] = true;
+            for (int j = 0; j < n; ++j)
+                if (!visited[j])
+                    pq.push({-(abs(ps[i][0] - ps[j][0]) + abs(ps[i][1] - ps[j][1])), j});
+            while (visited[pq.top().second])
+                pq.pop();
+            res -= pq.top().first;
+            i = pq.top().second;
+            pq.pop();
+        }
+        return res;
+    }
+};
