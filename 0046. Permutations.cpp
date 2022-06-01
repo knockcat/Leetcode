091974277754238035d5959d4ca099a3b3cb3690
VICKY <vishujoshi21nov2000@gmail.com>
2022-06-01 06:15:05 +0530

    Solution as on 01-06-2022 06:30 am

--- 0046. Permutations.cpp
+++ 0046. Permutations.cpp
@@ -17,3 +17,32 @@ public:
         return v;
     }
 };
+
+class Solution
+{
+public:
+    void helper(vector<int> &nums, vector<vector<int>> &res, int ind)
+    {
+        if (ind == nums.size())
+        {
+            res.push_back(nums);
+            return;
+        }
+
+        for (int i = ind; i < nums.size(); ++i)
+        {
+            swap(nums[i], nums[ind]);
+            helper(nums, res, ind + 1);
+            swap(nums[i], nums[ind]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> res;
+
+        helper(nums, res, 0);
+
+        return res;
+    }
+};
