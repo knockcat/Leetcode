e26f049426155f1ac0f5896ec0d800f09f034c62
VICKY <vishujoshi21nov2000@gmail.com>
2022-06-02 07:01:22 +0530

    Solution as on 02-06-2022 07:50 am

--- 0075. Sort Colors.cpp
+++ 0075. Sort Colors.cpp
@@ -5,6 +5,26 @@ class Solution
 public:
     void sortColors(vector<int> &nums)
     {
+        int zero = 0, start = 0, end = nums.size() - 1;
+
+        while (start <= end)
+        {
+            if (nums[start] == 0)
+                swap(nums[start++], nums[zero++]);
+            else if (nums[start] == 2)
+                swap(nums[end--], nums[start]);
+            else
+                ++start;
+        }
+    }
+};
+
+// Bad Solution
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
         sort(nums.begin(), nums.end());
     }
 };
