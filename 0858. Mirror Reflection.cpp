2c958e00ac3afb9883cda1fd1c20b6de3d731f2b
VICKY <vishujoshi21nov2000@gmail.com>
2022-08-14 23:18:19 +0530

    Solution as on 14-08-2022 11:20 pm

---
+++ 0858. Mirror Reflection.cpp
@@ -0,0 +1,15 @@
+// 858.✅ Mirror Reflection
+
+class Solution
+{
+public:
+    int mirrorReflection(int p, int q)
+    {
+        while (p % 2 == 0 && q % 2 == 0)
+        {
+            p /= 2;
+            q /= 2;
+        }
+        return 1 - p % 2 + q % 2;
+    }
+};
