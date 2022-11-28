dc2a06d333b44d9f4b3264d960b6ff56754c0b30
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2022-11-28 19:00:19 +0530

    Time: 994 ms (12.50%), Space: 160.9 MB (12.50%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -10,54 +10,13 @@
  */
 class Solution {
 public:
-    
-    ListNode* rev(ListNode* head){
-        ListNode* prev = nullptr, *curr = head;
-        while(curr != nullptr)
-        {
-            swap(curr->next,prev);
-            swap(prev,curr);
-        }
-        return prev;
-    }
-    
-    ListNode* reverseList(ListNode* head){
-        ListNode* prev = nullptr, *curr = head;
-        while(curr != nullptr)
-        {
-            ListNode* temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = temp;
-        }
-        return prev;
-    }
-    
-    void delete_Node(ListNode* head)
-    {
-        ListNode *maxNode = head,  *curr = head;
-        
-        while(curr && curr->next)
-        {
-            if(curr->next->val < maxNode->val)
-            {
-                ListNode* temp = curr->next;
-                curr->next = temp->next;
-                delete(temp);
-            }
-            else
-            {
-                curr = curr->next;
-                maxNode = curr;
-            }
-        }
-    }
-    
     ListNode* removeNodes(ListNode* head) {
-        
-        head = reverseList(head);
-        delete_Node(head);
-        head = reverseList(head);
+        if(!head or !head->next)
+            return head;
+        ListNode* newNode = removeNodes(head->next);
+        if(newNode->val > head->val)
+            return newNode;
+        head->next = newNode;
         return head;
     }
 };
