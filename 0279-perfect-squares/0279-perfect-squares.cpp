c47282c62df7ea7bbe644a451d7589198888aa63
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2022-11-28 08:53:04 +0530

    Time: 246 ms (62.77%), Space: 9.1 MB (63.70%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    
-    int helper(int n, vector<int>& dp){
-        
-        if(n <= 3)
-            return n;
-        if(dp[n] != -1)
-            return dp[n];
-        
-        int ans = n;
-        for(int i = 1; i*i <= n; ++i){
-            ans = min(ans, 1 + helper(n - (i*i), dp));
-            dp[n] = ans;
-        }
-        return ans;
-    }
-    
     int numSquares(int n) {
         
         vector<int> dp(n+1,-1);
-        int ans = helper(n,dp);
-        return ans;
         
+        dp[0] = 0;
+        
+        for(int i = 1; i<=n; ++i)
+        {
+            dp[i] = i;
+            for(int j = 1; j*j <= i; ++j)
+            {
+                int sq = j*j;
+                dp[i] = min(dp[i], 1 + dp[i-sq]);
+            }
+        }
+        return dp[n];
     }
 };
