3ba72bce98fb5e44a75e915d8d20c2f9804978ad
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2022-12-13 19:07:04 +0530

    Time: 241 ms (73.08%), Space: 55 MB (74.68%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     
-    int helper(int index, int buy, vector<int>& prices, int fee, vector<vector<int>>& dp)
-    {
-        if(index == prices.size())
-            return 0;
-        if(dp[index][buy] != -1)
-            return dp[index][buy];
-        if(buy)
-            return dp[index][buy] = max(-prices[index] + helper(index+1, 0, prices,fee,dp), helper(index+1, 1, prices,fee,dp));
-        return dp[index][buy] = max(prices[index] - fee + helper(index+1,1,prices,fee,dp), helper(index+1,0,prices,fee,dp));
-    }
-    
     int maxProfit(vector<int>& prices, int fee) {
         
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        vector<int> curr(2,0), ahead(2,0);
         
         for(int i = n-1; i>=0; --i)
         {
             for(int buy = 1; buy>=0; --buy)
             {
                 if(buy == 1)
-                    dp[i][buy] = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
+                    curr[buy] = max(-prices[i] + ahead[0], ahead[1]);
                 else
-                    dp[i][buy] = max(prices[i] - fee +  dp[i+1][1], dp[i+1][0]);
+                    curr[buy] = max(prices[i] - fee +  ahead[1], ahead[0]);
             }
+            ahead = curr;
         }
-        return dp[0][1];
+        return ahead[1];
     }
 };
