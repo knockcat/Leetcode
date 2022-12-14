eee2bf84de519b4e4f8a67f17f42c7449319f2c1
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2022-12-14 10:49:15 +0530

    Time: 364 ms (61.87%), Space: 10.8 MB (24.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,18 +3,41 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> dp(n+1,1);
+        vector<int> dp(n+1,1),hash(n);
         
+        int ans = 1, lastIndex = 0;
         for(int i= 0; i<n; ++i)
         {
+            hash[i] = i;
             for(int j = 0; j<i; ++j)
             {
-                if(nums[i] > nums[j])
-                    dp[i] = max(dp[i], dp[j] + 1);
+               if(nums[i] > nums[j]  and dp[j] + 1 > dp[i])
+               {
+                    dp[i] = dp[j] + 1;
+                    hash[i] = j;
+               }
             }
-            
+            if(dp[i] > ans)
+            {
+                ans = dp[i];
+                lastIndex = i;
+            }         
         }
         
-        return *max_element(dp.begin(),dp.end());
+        vector<int> lis;
+        lis.push_back(nums[lastIndex]);   
+        while(hash[lastIndex] != lastIndex)
+        {
+            lastIndex = hash[lastIndex];
+            lis.push_back(nums[lastIndex]);
+        }
+        
+        reverse(lis.begin(),lis.end());
+        for(auto itr : lis)
+            cout<<itr<<" ";
+        cout<<endl;
+        
+        return ans;
+        
     }
 };
