9b405a7338a9e728332a803b2646876149542df7
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-01-29 03:59:39 +0530

    Time: 12 ms (47.05%), Space: 8.5 MB (83.92%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -23,9 +23,26 @@ public:
     int minScoreTriangulation(vector<int>& values) {
         
         int n = values.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(n+1,vector<int>(n+1,INT_MAX));
         
-        return helper(1,n-1,values, dp);
+        for(int i = 0; i<n; ++i)
+            dp[i][i] = 0;
         
+        for(int i = n-1; i>=1; --i)
+        {
+            for(int j = i+1; j<n; ++j) 
+            {
+                int ans = INT_MAX;
+                for(int k = i; k < j; ++k)
+                {
+                    int steps = values[i-1] * values[k] * values[j] + dp[i][k] + dp[k+1][j];
+                    
+                    ans =  min(ans,steps);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        
+        return dp[1][n-1];
     }
 };
