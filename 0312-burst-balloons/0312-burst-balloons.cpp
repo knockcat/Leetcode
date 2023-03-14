58ccaec6c99b28111bc05f7373a04c3ea50b6ca6
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-03-14 19:25:27 +0530

    Time: 364 ms (78.83%), Space: 10.2 MB (82.68%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -25,8 +25,28 @@ public:
         nums.insert(nums.begin(),1);
         
         
-        vector<vector<int> >dp(n+1,vector<int>(n+1,-1));
-        return helper(1,n, nums, dp);
+        // vector<vector<int> >dp(n+1,vector<int>(n+1,-1));
+        // return helper(1,n, nums, dp);
         
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        
+        for(int i = n; i >= 1; --i)
+        {
+            for(int j = 1; j<=n; ++j)
+            {
+                if(i > j)
+                    continue;
+                int maxi = INT_MIN;
+                for(int idx = i; idx <= j; ++idx)
+                {
+                    int cost = nums[i-1] * nums[idx] * nums[j+1] + dp[i][idx-1] + dp[idx+1][j];
+                    maxi = max(maxi, cost);
+                }
+
+                dp[i][j] = maxi;
+            }
+        }
+        
+        return dp[1][n];
     }
 };
