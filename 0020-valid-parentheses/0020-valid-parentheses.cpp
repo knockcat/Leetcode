62438f2f456644392ff5103df26dbbd19b8e9269
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-04-10 18:21:13 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (12.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        
+        stack<char> st;
+        
+        for(auto itr : s)
+        {
+            if(itr == '(' or itr == '{' or itr == '[')
+                st.push(itr);
+            else if(!st.empty())
+            {
+                if(st.top() == '(' and itr != ')')
+                    return false;
+                else if(st.top() == '{' and itr != '}')
+                    return false;
+                else if(st.top() =='[' and itr != ']')
+                    return false;
+                
+                st.pop();
+            }
+            else
+                return false;
+        }
+        
+        return st.empty();
+        
+    }
+};
