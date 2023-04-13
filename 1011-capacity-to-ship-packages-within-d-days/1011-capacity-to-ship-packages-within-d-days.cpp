79518c5a85ff7770dd9d4fb89324fc6ff03df9b3
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-04-14 01:46:46 +0530

    Time: 99 ms (12.54%), Space: 31.1 MB (10.42%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,17 +1,21 @@
+#define ll long long int
 class Solution {
 public:
     
-    int binarySearch(vector<int>& weights, int days, int mid)
+    int helper(vector<int>& weights, int mid, int days)
     {
+        ll sum = 0;
         int cnt = 1;
         int n = weights.size();
-        int sum = 0;
+        
         for(int i = 0; i<n; ++i)
         {
-            if(sum + weights[i] > mid)
+            if(weights[i] > mid)
+                return false;
+            else if(sum + weights[i] > mid)
             {
-                ++cnt;
                 sum = weights[i];
+                ++cnt;
             }
             else
                 sum += weights[i];
@@ -20,23 +24,25 @@ public:
         return (cnt <= days);
     }
     
-    
     int shipWithinDays(vector<int>& weights, int days) {
         
-        int right = accumulate(weights.begin(),weights.end(),0);
-        int left = *max_element(weights.begin(),weights.end());
+        ll sum = accumulate(weights.begin(),weights.end(),0);
+        int mini = *min_element(weights.begin(),weights.end());
         
-        int ans = left;
-        while(left <= right)
+        ll low = mini , high = sum;
+        ll ans;
+        
+        while(low <= high)
         {
-            int mid = (right + left)/2;
-            if(binarySearch(weights,days,  mid))
+            ll mid = low + (high - low)/2;
+            
+            if(helper(weights,mid,days))
             {
-                ans =  mid;
-                right = mid - 1;
+                ans = mid;
+                high = mid - 1;
             }
             else
-                left = mid + 1;
+                low = mid + 1;
         }
         
         return ans;
