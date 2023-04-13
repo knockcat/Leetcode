baeee79786ab2dc4948290a3b7747e059a8f3937
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-04-14 00:25:55 +0530

    Time: 4 ms (38.99%), Space: 6.3 MB (82.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,22 +6,22 @@ public:
         
         for(auto itr : s)
         {
-          if(itr == '(' or itr == '{' or itr == '[')
-              st.push(itr);
-          else if(!st.empty())
-          {
-              if(st.top() == '(' and itr != ')')
-                  return false;
-              if(st.top() == '{' and itr != '}')
-                  return false;
-              if(st.top() == '[' and itr != ']')
-                  return false;
-              st.pop();
-          }
-          else
-              return false;
+            if(itr == '(' or itr == '{' or itr == '[')
+                st.push(itr);
+            else if(!st.empty())
+            {
+                if(st.top() == '(' and itr != ')')
+                   return false;
+                if(st.top() == '{' and itr != '}')
+                   return false;
+                if(st.top() == '[' and itr != ']')
+                   return false;
+                st.pop();
+            }
+            else
+                   return false;
         }
-        
+                   
         return st.empty();
         
     }
