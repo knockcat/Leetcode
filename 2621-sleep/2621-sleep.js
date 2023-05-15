28ff41d62e2ba9128e2329eb5835735d615b13b0
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-05-15 08:59:41 +0530

    Time: 65 ms (25.43%), Space: 42 MB (27.35%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    await new Promise(resolve => setTimeout(resolve, millis));
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
