07d96fc5bda5d1f5c3c4ea8a61a9da51228ac017
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-06-04 10:01:07 +0530

    Time: 30 ms (36.74%), Space: 13.6 MB (98.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,88 +1,72 @@
-template <typename T>
-class DSU
-{
-public:
-    int N;
-    vector<T> rank, parent, size;
-    DSU(int n)
-    {
-        N = n;
-        rank.resize(n + 1, 0);
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-
-        for (T i = 0; i <= n; ++i)
+class DSU{
+    private:
+        vector<int> rank, parent, size;
+    public:
+        DSU(int n)
         {
-            parent[i] = i;
+            rank.resize(n+1, 0);
+            parent.resize(n+1);
+            size.resize(n+1, 1);
+            
+            for(int i = 0; i <= n; ++i)
+                parent[i] = i;
         }
-    }
-
-    template <typename X>
-    X findParent(X u)
-    {
-        if (u == parent[u])
-            return u;
-        return parent[u] = findParent(parent[u]);
-    }
-
-    // Not be using unionByRank
-    // but yes for future reference
-    template <typename X>
-    void unionByRank(X u, X v)
-    {
-        int nodeX = findParent(u);
-        int nodeY = findParent(v);
-
-        if (nodeX == nodeY)
-            return;
-
-        if (rank[nodeX] < rank[nodeY])
+    
+        int findPar(int u)
         {
-            parent[nodeX] = nodeY;
+            if(parent[u] == u)
+                return parent[u];
+            return parent[u] = findPar(parent[u]);
         }
-        else if (rank[nodeY] < rank[nodeX])
+    
+        void unionByRank(int u, int v)
         {
-            parent[nodeY] = nodeX;
+            int parU = findPar(u);
+            int parV = findPar(v);
+            
+            if(parU == parV)
+                return;
+            
+            if(rank[parU] < rank[parV])
+            {
+                parent[parU] = parV;
+            }
+            else if(rank[parV] < rank[parU])
+            {
+                parent[parV] = parU;
+            }
+            else
+            {
+                parent[parV] = parU;
+                ++rank[parU];
+            }
         }
-        else
+    
+        void unionBySize(int u, int v)
         {
-            parent[nodeY] = nodeX;
-            ++rank[nodeX];
+            int parU = findPar(u);
+            int parV = findPar(v);
+            
+            if(parU == parV)
+                 return;
+            
+            if(size[parU] < size[parV])
+            {
+                parent[parU] = parV;
+                size[parV] += size[parU];
+            }
+            else
+            {
+                parent[parV] = parU;
+                size[parU] += size[parV];
+            }
         }
-    }
-
-    void unionBySize(int u, int v)
-    {
-        int nodeX = findParent(u);
-        int nodeY = findParent(v);
-
-        if (nodeX == nodeY)
-            return;
-
-        if (size[nodeX] < size[nodeY])
+    
+        bool isSame(int u, int v)
         {
-            parent[nodeX] = nodeY;
-            size[nodeY] += size[nodeX];
+            return findPar(u) == findPar(v);
         }
-        else
-        {
-            parent[nodeY] = nodeX;
-            size[nodeX] += size[nodeY];
-        }
-    }
-
-    template <typename X>
-    bool isSame(X u, X v)
-    {
-        return findParent(u) == findParent(v);
-    }
 };
-// DSU<int> dsu(n);
-
-// dsu.unionByRank(u, v);
-// dsu.unionBySize(u,v);
-// dsu.isSame(u, v) bool
-
 
 class Solution {
 public:
@@ -90,24 +74,27 @@ public:
         
         int n = isConnected.size();
         int m = isConnected[0].size();
-        int provinces = 0;
         
-        DSU<int> dsu(n);
-        for(int i = 0; i< n; ++i)
-        {
-            for(int j = 0; j<m; ++j)
-            {
-                if(isConnected[i][j])
-                    dsu.unionBySize(i,j);
-            }
-        }
-    
+        DSU dsu(n);
+        
         for(int i = 0; i < n; ++i)
         {
-            if(dsu.findParent(i) == i)
-                ++provinces;
+            for(int j = 0; j < m; ++j)
+            {
+                if(isConnected[i][j] == 1)
+                    dsu.unionBySize(i+1, j+1);
+            }
         }
-
-        return provinces;
+        
+        int cnt = 0;
+        
+        for(int i = 1; i <= n; ++i)
+        {
+            if(dsu.findPar(i) == i)
+                ++cnt;
+        }
+        
+        return cnt;
+        
     }
 };
