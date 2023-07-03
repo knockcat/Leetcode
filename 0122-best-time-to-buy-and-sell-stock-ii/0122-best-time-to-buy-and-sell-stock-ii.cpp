e3e1eac3a92036e325f9bf7cc5f22ff1e7d248f9
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-07-03 07:38:59 +0530

    Time: 7 ms (75.00%), Space: 13.1 MB (37.68%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,38 +1,21 @@
 class Solution {
-    
-private:
-    int helper(int idx, int buy, int n, vector<int>& prices, vector<vector<int>>& dp)
-    {
-        if(idx == n)
-            return 0;
-        
-        if(dp[idx][buy] != -1)
-            return dp[idx][buy];
-        
-        int take = 0, notTake = 0, notTake2 = 0, take2 = 0;
-        
-        if(buy)
-        {
-            take = -prices[idx] + helper(idx+1, 0, n,  prices, dp);
-            notTake = helper(idx+1, buy, n, prices, dp);
-        }
-        else
-        {
-            take2 = prices[idx] + helper(idx+1, 1, n, prices, dp);
-            notTake2 = helper(idx+1, buy, n, prices, dp);
-        }
-        
-        return dp[idx][buy] = max({take, notTake, take2, notTake2});
-        
-    }
 public:
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
         
-        vector<vector<int>> dp(n, vector<int>(2, -1));
+        int currBuy, currNotBuy, aheadBuy = 0, aheadNotBuy = 0;
         
-        return helper(0, 1, n, prices,dp);
+        for(int i = n-1; i >=0 ; --i)
+        {
+            currBuy = max(-prices[i] + aheadNotBuy, aheadBuy);
+            currNotBuy = max(prices[i] + aheadBuy, aheadNotBuy);
+            
+            aheadBuy = currBuy;
+            aheadNotBuy = currNotBuy;
+        }
+        
+        return aheadBuy;
         
     }
 };
