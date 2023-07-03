3b5c73334cd1d6451053ee4122556922a65e06fe
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-07-03 07:20:27 +0530

    Time: 12 ms (80.94%), Space: 10.7 MB (50.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,19 +6,16 @@ public:
         
         unordered_map<int,int> mp;
         
-        for(int i = 0;  i < n; ++i)
-            mp[nums[i]] = i;
-        
         for(int i = 0; i < n; ++i)
         {
-            if(mp.find(target-nums[i]) != mp.end())
+            if(mp.find(target - nums[i]) != mp.end())
             {
-                if(i != mp[target - nums[i]])
-                    return {i, mp[target - nums[i]]};
+                return {i, mp[target - nums[i]]};
             }
+            mp[nums[i]] = i;
         }
         
-        return {-1,-1};
+        return {};
         
     }
 };
