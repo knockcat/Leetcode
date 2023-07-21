b05ce7e5e8334ef21eaef9f91f92a234ccbe7eb7
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-07-21 09:03:00 +0530

    Time: 1371 ms (23.71%), Space: 292.3 MB (8.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,43 +1,35 @@
 class Solution {
+
+private:
+    int helper(int idx, int prev, int n, vector<int>& nums, vector<vector<int>>& dp)
+        {
+            if(idx == n)
+                return 0;
+
+            if(dp[idx][prev+1] != -1)
+            {
+                return dp[idx][prev+1];
+            }
+
+            int notTake = helper(idx+1, prev, n, nums, dp);
+
+            int take = 0;
+
+            if(prev == -1 or nums[idx] > nums[prev])
+                take = 1 + helper(idx+1, idx, n, nums, dp);
+
+            return dp[idx][prev+1] = max(take, notTake);
+    }    
+    
+    
 public:
     int lengthOfLIS(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> dp(n+1,1),hash(n);
         
-        int ans = 1, lastIndex = 0;
-        for(int i= 0; i<n; ++i)
-        {
-            hash[i] = i;
-            for(int j = 0; j<i; ++j)
-            {
-               if(nums[i] > nums[j]  and dp[j] + 1 > dp[i])
-               {
-                    dp[i] = dp[j] + 1;
-                    hash[i] = j;
-               }
-            }
-            if(dp[i] > ans)
-            {
-                ans = dp[i];
-                lastIndex = i;
-            }         
-        }
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
         
-        vector<int> lis;
-        lis.push_back(nums[lastIndex]);   
-        while(hash[lastIndex] != lastIndex)
-        {
-            lastIndex = hash[lastIndex];
-            lis.push_back(nums[lastIndex]);
-        }
-        
-        reverse(lis.begin(),lis.end());
-        for(auto itr : lis)
-            cout<<itr<<" ";
-        cout<<endl;
-        
-        return ans;
+        return helper(0, -1, n, nums, dp);
         
     }
 };
