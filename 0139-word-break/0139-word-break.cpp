2dd04a5c848d8ad0ed8372b3f13bb1362fc27a95
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-08-02 00:56:37 +0530

    Time: 7 ms (74.20%), Space: 8.1 MB (77.31%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,43 +1,35 @@
 class Solution {
-    
-private:
-    bool helper(int idx, int n, string& str, unordered_set<string>& st, vector<int>& dp)
-    {
-        if(idx == n)
-            return true;
-        
-        if(dp[idx] != -1)
-            return dp[idx];
-        
-        string temp = "";
-        
-        for(int i = idx; i < n; ++i)
-        {
-            temp += str[i];
-            
-            if(st.find(temp) != st.end())
-            {
-                if(helper(i+1, n, str, st, dp))
-                    return dp[idx] = true;
-            }
-        }
-        
-        return dp[idx] = false;
-    }
-    
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         
         int n = s.size();
         
-        vector<int> dp(n+1 , -1);
+        int mod = 1e9 + 7;
+        
+        vector<int> dp(n+1, 0);
         
         unordered_set<string> st;
         
-        for(auto itr : wordDict)
+        for(auto& itr : wordDict)
             st.insert(itr);
         
-        return helper(0, n, s, st, dp);
-        
+        for(int i = 0; i < n; ++i)
+        {
+            string temp;
+            for(int j = i; j < n; ++j)
+            {
+                temp += s[j];
+                
+                if(st.find(temp) != st.end())
+                {
+                    if(i > 0)
+                        dp[j] =  ((dp[j] % mod + dp[i-1]) % mod) % mod;
+                    else
+                        dp[j] = 1;
+                }
+            }
+        }
+    
+        return dp[n-1];
     }
 };
