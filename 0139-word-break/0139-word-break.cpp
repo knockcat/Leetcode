c114daf5b290942b3cad6071a21ffe084458acc9
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-08-04 23:40:55 +0530

    Time: 7 ms (73.42%), Space: 8.6 MB (72.71%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,38 +1,37 @@
 class Solution {
+    
+private:
+    bool helper(int idx, int n, string& s, unordered_set<string>& used, vector<int>& dp)
+    {
+        if(idx == n)
+            return true;
+        
+        if(dp[idx] != -1)
+            return dp[idx];
+        
+        string temp;
+        for(int i = idx; i < n; ++i)
+        {
+            temp += s[i];
+            
+            if(used.find(temp) != used.end())
+            {
+                if(helper(i+1, n, s, used, dp))
+                    return dp[idx] = true;
+            }
+        }
+        return dp[idx] = false;
+    }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         
+        unordered_set<string> used(wordDict.begin(), wordDict.end());
+        
         int n = s.size();
         
-        int mod = 1e9 + 7;
+        vector<int> dp(n+1, -1);
         
-        vector<int> dp(n+1, 0);
+        return helper(0, n, s, used, dp);
         
-        unordered_set<string> st;
-        
-        for(auto& itr : wordDict)
-            st.insert(itr);
-        
-        for(int i = 0; i < n; ++i)
-        {
-            string temp;
-            for(int j = i; j < n; ++j)
-            {
-                temp += s[j];
-                
-                if(st.find(temp) != st.end())
-                {
-                    if(i > 0)
-                    {
-                        if(!dp[j])
-                            dp[j] = dp[i-1];
-                    }
-                    else
-                        dp[j] = 1;
-                }
-            }
-        }
-    
-        return dp[n-1];
     }
 };
