e7e62eb5bc9208ae5c9ca656a6008ed35235f3aa
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-08-11 21:13:13 +0530

    Time: 651 ms (12.07%), Space: 106.8 MB (10.07%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
+    
+    const int mod = 1e9 + 7;
+    
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
         int n = nums.size();
         
-        int mod = 1e9 + 7;
-        
         vector<vector<int>> ans;
         
-        set<vector<int>> used;
-        
-        sort(begin(nums), end(nums));
+        sort(nums.begin(), nums.end());
         
         for(int i = 0; i < n; ++i)
         {
@@ -20,24 +19,29 @@ public:
                 
                 while(k < l)
                 {
-                    long long sum = (((((nums[i] % mod) + nums[j])%mod + nums[k])% mod + nums[l])% mod) % mod;
+                    long long sum = (((((nums[i] % mod) + nums[j])% mod + nums[k])%mod + nums[l])%mod)%mod;
                     
                     if(sum == target)
                     {
-                        used.insert({nums[i], nums[j], nums[k], nums[l]});
-                        ++k,--l;
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        ++k, --l;
                     }
-                    else if(sum < target)
-                        ++k;
-                    else
+                    else if(sum > target)
+                    {
                         --l;
+                    }
+                    else
+                    {
+                        ++k;
+                    }
                 }
             }
         }
         
-        for(auto itr : used)
-            ans.push_back(itr);
-            
+        sort(ans.begin(), ans.end());
+        
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        
         return ans;
     }
 };
