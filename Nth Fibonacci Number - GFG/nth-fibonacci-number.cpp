4734e28ec89319574ab32afd7290ba4b6a0bbfe8
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-08-14 21:08:24 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -6,30 +6,67 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 class Solution {
-  public:
     
-    const int mod = 1e9+7;
+  private:
   
+   const int mod = 1e9 + 7;
+   
+    vector<vector<long long>> matrixMultiplication(vector<vector<long long>> mat1, vector<vector<long long>> mat2)
+    {
+        vector<vector<long long>> ans(2, vector<long long>(2));
+        
+        for(int i = 0; i < 2; ++i)
+        {
+            for(int j = 0; j < 2;  ++j)
+            {
+                for(int k =0 ; k < 2 ; ++k)
+                {
+                    ans[i][j] +=  (long long)(mat1[i][k]%mod * mat2[k][j]%mod)%mod;
+                    ans[i][j] %= mod;
+                }
+            }
+        }
+        
+        return ans;
+    }
+   
+  
+    vector<vector<long long>> expo(vector<vector<long long>>& mat, int n)
+    {
+        
+        if(n == 1)
+            return mat;
+        
+        vector<vector<long long>> temp = mat;
+       
+        
+        while(n > 0)
+        {
+            if(n & 1)
+            {
+                temp = matrixMultiplication(mat, temp);
+            }
+            
+            mat = matrixMultiplication(mat, mat);
+            
+            n >>= 1;
+        }
+        
+        return temp;
+    }
+    
+  public:
     int nthFibonacci(int n){
         // code here
         
-        int first = 0, second = 1;
-        
-        if(n == 1)
+        if(n == 0)
             return 0;
-        if(n == 2)
-            return 1;
-            
-        int ans = second;
         
-        for(int i = 2; i <= n; ++i)
-        {
-            ans = (first + second)%mod;
-            first = second;
-            second = ans;
-        }
+        vector<vector<long long>> mat = {{1,1},{1,0}};
         
-        return ans % mod;
+        vector<vector<long long>> ans = expo(mat, n-1);
+        
+        return ans[0][1]%mod;
     }
 };
 
