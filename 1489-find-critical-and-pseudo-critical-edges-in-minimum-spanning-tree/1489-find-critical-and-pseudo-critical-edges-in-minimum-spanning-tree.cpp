b8ebea4643ea7f26b3b0bcf02c97d1b03ff1ff29
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-08-21 22:49:30 +0530

    Time: 119 ms (46.23%), Space: 24 MB (18.77%) - LeetHub

--- 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
@@ -1,45 +1,147 @@
 class Solution {
 public:
-    vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
-        vector<vector<pair<int, int>>> g(n);
-        for (auto &e : edges) {
-            int u = e[0], v = e[1], w = e[2];
-            g[u].emplace_back(v, w);
-            g[v].emplace_back(u, w);
-        }
-        vector<vector<int>> ans(2);
-        for (int i = 0; i < edges.size(); i++) {
-            auto &e = edges[i];
-            int u = e[0], v = e[1], w = e[2];
-            int m = minimax(g, u, v);
-            if (w < m) {
-                ans[0].push_back(i);
-            } else if (w == m) {
-                ans[1].push_back(i);
+    
+    int N;
+    
+    class DSU{
+        
+        public :
+            vector<int> parent, size, rank;
+        
+            DSU(int n)
+            { 
+                parent.resize(n+1);
+                size.resize(n+1, 1);
+                rank.resize(n+1, 0);
+                
+                for(int i = 0; i <= n; ++i)
+                    parent[i] = i;
             }
+        
+            int findParent(int u)
+            {
+                if(u == parent[u])
+                    return u;
+                
+                return parent[u] = findParent(parent[u]);
+            }
+        
+            void unionByRank(int u, int v)
+            {
+                int nodeX = findParent(u);
+                int nodeY = findParent(v);
+                
+                if(nodeX == nodeY)
+                    return;
+                
+                if(rank[nodeX] < rank[nodeY])
+                {
+                    parent[nodeX] = nodeY;
+                }
+                else if(rank[nodeY] < rank[nodeX])
+                {
+                    parent[nodeY] = nodeX;
+                }
+                else
+                {
+                    parent[nodeY] = nodeX;
+                    ++rank[nodeY];
+                }
+            }
+        
+            void unionBySize(int u, int v)
+            {
+                int nodeX = findParent(u);
+                int nodeY = findParent(v);
+                
+                if(nodeX == nodeY)
+                    return;
+                
+                if(size[nodeX] < size[nodeY])
+                {
+                    parent[nodeX] = nodeY;
+                    size[nodeY] += size[nodeX];
+                }
+                else
+                {
+                    parent[nodeY] = nodeX;
+                    size[nodeY] += size[nodeX];
+                }
+            }
+        
+            bool isSame(int u, int v)
+            {
+                return findParent(u) == findParent(v);
+            }
+    };
+    
+    int kruskals(vector<vector<int>>& edges, int skip_edge, int add_edge)
+    {
+        DSU dsu(N);
+        
+        int edges_cnt = 0;
+        
+        int mstWeight = 0;
+        
+        if(add_edge != -1)
+        {
+            dsu.unionBySize(edges[add_edge][0], edges[add_edge][1]);
+            mstWeight += edges[add_edge][2];
+            ++edges_cnt;
         }
-        return ans;
-    }
-	
-    int minimax(vector<vector<pair<int, int>>> &g, int src, int dst) {
-        vector<int> dist(g.size(), 1e9);
-        priority_queue<pair<int, int>> pq;
-        pq.emplace(dist[src] = 0, src);
-        while (!pq.empty()) {
-            auto [pri, u] = pq.top(); pq.pop();
-            if (-pri > dist[u]) {
+        
+        for(int i = 0; i < edges.size(); ++i)
+        {
+            if(i == skip_edge)
+                continue;
+            
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+            
+            if(dsu.isSame(u, v))
+            {
                 continue;
             }
-            for (auto &[v, w] : g[u]) {
-                if (u == src && v == dst) {
-                    continue;
-                }
-                if (dist[v] > max(dist[u], w)) {
-                    dist[v] = max(dist[u], w);
-                    pq.emplace(-dist[v], v);
-                }
+            else
+            {
+                dsu.unionBySize(u, v);
+                mstWeight += wt;
+                ++edges_cnt;
             }
         }
-        return dist[dst];
+        
+        if(edges_cnt != N-1)
+            return INT_MAX;
+        
+        return mstWeight;
+    }
+    
+    vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
+        
+        // JAI SHREE RAM 
+        
+        N = n;
+        
+        for(int i = 0; i < edges.size(); ++i)
+            edges[i].push_back(i);
+        
+        sort(edges.begin(), edges.end(), [&](const auto &a, const auto &b){
+            return a[2] < b[2];
+        });
+        
+        int mstWeight = kruskals(edges, -1, -1);
+        
+        vector<int> critical, pseudoCritical;
+        
+        for(int i = 0; i < edges.size(); ++i)
+        {
+            if(kruskals(edges, i, -1) > mstWeight)
+                critical.push_back(edges[i].back());
+            else if(kruskals(edges, -1, i) == mstWeight)
+                pseudoCritical.push_back(edges[i].back());
+        }
+        
+        return {critical, pseudoCritical};
     }
 };
