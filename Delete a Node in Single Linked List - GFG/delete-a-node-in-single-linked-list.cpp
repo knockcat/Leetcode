4a2786cdc1283b5ca17f9220b82972e9d4510f87
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-08-30 07:08:54 +0530

    Added solution - LeetHub

---
+++ Delete a Node in Single Linked List - GFG/delete-a-node-in-single-linked-list.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref,
+           int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+/* Function to get the middle of the linked list*/
+struct Node* deleteNode(struct Node *head,int );
+
+void printList(Node *head)
+{
+   while (head != NULL)
+   {
+      cout << head->data << " ";
+      head = head->next;
+   }
+   cout << "\n";
+}
+
+/* Driver program to test above function*/
+int main()
+{
+  int T, i, n, l;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+
+	int kk;
+	cin>>kk;
+    head = deleteNode(head,kk);
+    printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+/*You are required to complete below method*/
+Node* deleteNode(Node *head,int x)
+{
+    //Your code here
+    
+    if(x == 1)
+        return head->next;
+        
+    Node* ptr = head;
+    
+    int cnt = 1;
+        
+    while(ptr and cnt < x-1)
+    {
+        ++cnt;
+        ptr = ptr->next;
+    }
+    
+    if(ptr->next->next)
+        ptr->next = ptr->next->next;
+    else
+        ptr->next = nullptr;
+        
+    return head;
+}
