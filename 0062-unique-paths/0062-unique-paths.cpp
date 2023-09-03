7ffbb8cfcc0e781cd265c5ab8aa7806d9fb57375
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-09-03 09:12:17 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (6.60%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
-public:
     
-    int helper(int i , int j, int n , int m, vector<vector<int>>& dp)
+private:
+    int helper(int i, int j, int n, int m, vector<vector<int>>& dp)
     {
         if(i == n-1 and j == m-1)
             return 1;
@@ -9,32 +9,21 @@ public:
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        if(i >= n or j >= m)
-            return 0;
+        int down = 0, right = 0;
         
-        return dp[i][j] = helper(i+1, j, n, m, dp) + helper(i, j+1, n, m, dp);
+        if(i+1 < n)
+            down = helper(i+1, j, n, m, dp);
+        if(j+1 < m)
+            right = helper(i, j+1, n, m, dp);
+            
+        return dp[i][j] = down + right;
     }
-    
+public:
     int uniquePaths(int m, int n) {
         
-        // vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+        vector<vector<int> > dp(m+1, vector<int>(n+1, -1));
         
-        // return helper(0,0,m ,n, dp);
+        return helper(0, 0, m , n, dp);
         
-        vector<int> prev(n+1, 0), curr(n+1, 0);
-        
-        for(int i = m-1; i>=0 ; --i)
-        {
-            for(int j = n-1; j >= 0; --j)
-            {
-                if(i == m-1 and j == n-1)
-                    curr[j] = 1;
-                else
-                    curr[j] = prev[j] + curr[j+1];
-            }
-            prev = curr;
-        }
-        
-        return prev[0];
     }
 };
