4f07aa239d163549c628acca42c69eabd9924c8e
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-09-04 09:53:43 +0530

    Time: 29 ms (76.07%), Space: 28.2 MB (21.27%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -4,20 +4,15 @@ public:
         
         vector<int> songFreq(60, 0);
         
+        int counter = 0;
+        
         for(auto& songTime : time)
         {
+            counter += songFreq[(60 - (songTime % 60)) % 60];
+            
             ++songFreq[songTime%60];
         }
         
-        long long counter = 0;
-        
-        for(int i = 1; i < 30; ++i)
-        {
-            counter += (songFreq[i] * 1LL * songFreq[60 - i]);
-        }
-        
-        counter += ((songFreq[0] * 1LL * (songFreq[0]-1))/2) +  ((songFreq[30] * 1LL *(songFreq[30]-1)/2));
-        
-        return (int) counter;
+        return counter;
     }
 };
