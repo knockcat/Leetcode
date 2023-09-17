7e102a913fe708bf91208d41b133d9132d75d87d
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-09-17 14:23:06 +0530

    Time: 144 ms (45.26%), Space: 20.2 MB (37.90%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.cpp
@@ -1,29 +1,48 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& h) {
-    int si = h.size(), sj = h[0].size();
-    vector<vector<int>> eff(si, vector<int>(sj, 1000000));
-    vector<int> dir{0, 1, 0, -1, 0};
-    auto comp = [&](const array<int, 3> &p1, const array<int, 3> &p2) { return p1[0] >= p2[0]; };
-    priority_queue<array<int, 3>, vector<array<int, 3>>, decltype(comp)> pq(comp);
-    eff[0][0] = 0;
-    pq.push({0, 0, 0});
-    while(!pq.empty()) {
-       auto [e, i, j] = pq.top();
-       pq.pop();
-       if (i == si - 1 && j == sj - 1)
-           break;
-       for (auto d = 0; d < 4; ++d) {
-           int x = i + dir[d], y = j + dir[d + 1];
-           if (x >= 0 && y >= 0 && x < si && y < sj) {
-               int new_eff = max(eff[i][j], abs(h[i][j] - h[x][y]));
-               if (new_eff < eff[x][y]) {
-                   eff[x][y] = new_eff;
-                   pq.push({new_eff, x, y});
-               }
-           }
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        
+        int n = heights.size();
+        int m = heights[0].size();
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>> > pq;
+        
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        
+        dist[0][0] = 0;
+        
+        pq.push({0, 0, 0});
+        
+        vector<int> dx = {0, +1, 0, -1};
+        vector<int> dy = {+1, 0, -1, 0};
+        
+        while(!pq.empty())
+        {
+            auto currRoute = pq.top();
+            pq.pop();
+            
+            int currDiff = get<0> (currRoute);
+            int x = get<1> (currRoute);
+            int y = get<2> (currRoute);
+            
+            for(int i = 0; i < 4; ++i)
+            {
+                int newx = dx[i] + x;
+                int newy = dy[i] + y;
+                
+                if(newx >= 0 and newy >= 0 and newx < n and newy < m)
+                {
+                    int diff = max(currDiff, abs(heights[x][y] - heights[newx][newy]));
+                    
+                    if(diff < dist[newx][newy])
+                    {
+                        pq.push({diff, newx, newy});
+                        dist[newx][newy] = diff;
+                    }
+                }
+            }
         }
+        
+        return dist[n-1][m-1];
+        
     }
-    return eff[si - 1][sj - 1];
-}
 };
