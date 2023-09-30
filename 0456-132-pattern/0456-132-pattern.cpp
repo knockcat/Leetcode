a1d4cf6e0af96b9255ac9567e56a6203068a33da
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-09-30 23:47:50 +0530

    Time: 58 ms (92.94%), Space: 48.9 MB (37.36%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -4,25 +4,22 @@ public:
         
         int n = nums.size();
         
-        vector<int> prefMin(n);
-        
-        prefMin[0] = nums[0];
-        
-        for(int i = 1; i < n; ++i)
-            prefMin[i] = min(nums[i], prefMin[i-1]);
+        int s3 = INT_MIN;
         
         stack<int> st;
         
         for(int i = n-1; i >= 0; --i)
         {
-            if(nums[i] > prefMin[i])
+            if(nums[i] < s3)
+                return true;
+            
+            while(!st.empty() and st.top() < nums[i])
             {
-                while(!st.empty() and st.top() <= prefMin[i])
-                    st.pop();
-                if(!st.empty() and nums[i] > st.top())
-                    return true;
-                st.push(nums[i]);
+                s3 = st.top();
+                st.pop();
             }
+            
+            st.push(nums[i]);
         }
         
         return false;
