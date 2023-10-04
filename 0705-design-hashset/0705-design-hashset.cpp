2491298c653e764c8398c963c99fd8415ddcae79
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-10-04 21:42:26 +0530

    Time: 91 ms (36.72%), Space: 50.6 MB (31.53%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,22 +1,55 @@
 class MyHashSet {
 public:
     
-    unordered_map<int,int> mp;
+    vector<list<int>> hash;
+    int size = 10001;
     
     MyHashSet() {
-        
+        hash.resize(size);
     }
     
     void add(int key) {
-        ++mp[key];
+        
+        int idx = key % size;
+        
+        for(auto& itr : hash[idx])
+        {
+            if(itr == key)
+            {
+                return;
+            }
+        }
+        
+        hash[idx].push_back(key);
     }
     
     void remove(int key) {
-        mp.erase(key);
+        
+        int idx = key % size;
+        
+        for(auto itr = hash[idx].begin(); itr != hash[idx].end(); ++itr)
+        {
+            if(*itr == key)
+            {
+                hash[idx].erase(itr);
+                return;
+            }
+        }
+        
     }
     
     bool contains(int key) {
-        return mp[key] > 0;
+        
+        int idx = key % size;
+        
+        for(auto& itr : hash[idx])
+        {
+            if(itr == key)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
 
