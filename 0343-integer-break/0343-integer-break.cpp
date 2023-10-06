7b808845d704f00c23cdf302fc3efd715aef144a
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-10-06 19:42:44 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (6.83%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     
-    int ans;
-    
-    void helper(int idx, int prod, int n, int k)
+    int helper(int idx, int sum, int n, vector<vector<int>>& dp)
     {
-       if(n == 0 and k >= 2)
-       {
-           ans = max(ans, prod);
-           return;
-       }
+        if(sum == 0)
+           return 1;
         
-       if(n-idx >= 0)
-       {
-           helper(idx, prod*idx, n-idx, k+1);
-           helper(idx+1, prod, n, k);
-       }      
-      
+        if(sum < 0 or idx >= n)
+            return 0;
+        
+        if(dp[idx][sum] != -1)
+            return dp[idx][sum];
+        
+        int res = helper(idx+1, sum, n, dp);
+        
+        dp[idx][sum] = res = max(res, idx * helper(idx, sum-idx, n, dp));
+        
+        return res;
     }
     
     int integerBreak(int n) {
-       
-       ans = 1; 
         
-       helper(1, 1, n, 0);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
         
-       return ans; 
+        return helper(1, n, n, dp);
+        
     }
 };
