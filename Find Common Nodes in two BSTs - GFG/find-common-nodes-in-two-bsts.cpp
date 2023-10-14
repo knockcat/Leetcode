b8bfc2ace00229d9ac2f9f8031852e71492a18e4
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-10-14 22:19:22 +0530

    Added solution - LeetHub

---
+++ Find Common Nodes in two BSTs - GFG/find-common-nodes-in-two-bsts.cpp
@@ -0,0 +1,149 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find the nodes that are common in both BST. 
+    
+    map<int,int> mp;
+    
+    void helper(Node* root)
+    {
+        if(root)
+        {
+            helper(root->left);
+            ++mp[root->data];
+            helper(root->right);
+        }
+    }
+    
+    vector <int> findCommon(Node *root1, Node *root2)
+    {
+     //Your code here
+     
+        helper(root1);
+        helper(root2);
+        
+        vector<int> ans;
+        
+        for(auto& itr : mp)
+        {
+            if(itr.second > 1)
+                ans.push_back(itr.first);
+        }
+        
+        return ans;
+        
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root1 = buildTree(s);
+
+        getline(cin,s);
+        Node* root2 = buildTree(s);
+        Solution ob;
+        vector <int> res = ob.findCommon(root1, root2);
+        for (int i : res)
+            cout << i << " ";
+        cout<< endl;
+    }
+
+	return 1;
+}
+// } Driver Code Ends
