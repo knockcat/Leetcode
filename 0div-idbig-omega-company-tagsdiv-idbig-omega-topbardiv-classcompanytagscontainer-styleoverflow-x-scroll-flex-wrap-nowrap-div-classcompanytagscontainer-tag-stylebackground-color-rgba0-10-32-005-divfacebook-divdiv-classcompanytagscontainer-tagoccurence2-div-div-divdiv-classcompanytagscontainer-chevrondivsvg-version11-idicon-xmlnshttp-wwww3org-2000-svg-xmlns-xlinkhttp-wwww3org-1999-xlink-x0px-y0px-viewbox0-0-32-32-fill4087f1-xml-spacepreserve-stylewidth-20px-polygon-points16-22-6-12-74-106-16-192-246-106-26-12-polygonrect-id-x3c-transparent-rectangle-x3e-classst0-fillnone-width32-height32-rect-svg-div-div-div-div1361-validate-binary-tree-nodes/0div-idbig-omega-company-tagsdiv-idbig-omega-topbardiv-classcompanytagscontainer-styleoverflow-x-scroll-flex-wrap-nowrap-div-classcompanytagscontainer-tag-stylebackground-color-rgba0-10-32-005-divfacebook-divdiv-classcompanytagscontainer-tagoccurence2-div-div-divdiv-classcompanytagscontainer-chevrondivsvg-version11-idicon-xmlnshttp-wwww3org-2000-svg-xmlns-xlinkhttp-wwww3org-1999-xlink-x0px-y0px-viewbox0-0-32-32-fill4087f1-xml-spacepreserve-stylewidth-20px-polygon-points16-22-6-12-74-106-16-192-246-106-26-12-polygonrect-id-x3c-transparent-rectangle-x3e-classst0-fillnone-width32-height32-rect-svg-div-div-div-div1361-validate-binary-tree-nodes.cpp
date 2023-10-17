dd51f2c6654090d3956c0758e09f662cc366fa50
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-10-17 22:30:23 +0530

    Time: 39 ms (59.88%), Space: 34.1 MB (51.67%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1361-validate-binary-tree-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1361-validate-binary-tree-nodes.cpp
@@ -0,0 +1,115 @@
+class DSU{
+  private:
+    vector<int> parent, size, rank;
+  public:
+    DSU(int n)
+    {
+        rank.resize(n+1, 0);
+        size.resize(n+1, 1);
+        parent.resize(n+1);
+        
+        for(int i = 0; i <= n; ++i)
+            parent[i] = i;
+    }
+    
+    int findParent(int u)
+    {
+        if(parent[u] == u)
+            return u;
+        return parent[u] = findParent(parent[u]);
+    }
+    
+    void unionByRank(int u, int v)
+    {
+        int parU = findParent(u);
+        int parV = findParent(v);
+        
+        if(parU == parV)
+            return;
+        
+        if(rank[parU] < rank[parV])
+        {
+            parent[parU] = parV;
+        }
+        else if(rank[parV] < rank[parU])
+        {
+            parent[parV] = parU;
+        }
+        else
+        {
+            parent[parV] = parU;
+            ++rank[parU];
+        }
+    }
+    
+    void unionBySize(int u, int v)
+    {
+        int parU = findParent(u);
+        int parV = findParent(v);
+        
+        if(parU == parV)
+            return;
+        
+        if(size[parU] < size[parV])
+        {
+            parent[parU] = parV;
+            size[parV] += size[parU];
+        }
+        else
+        {
+            parent[parV] = parU;
+            size[parU] += size[parV];
+        }
+    }
+    
+    bool isSame(int u, int v)
+    {
+        return findParent(u) == findParent(v);
+    }
+    
+};
+
+
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        
+        vector<int> indegree(n+1, 0);
+        
+        DSU dsu(n);
+        
+        for(int i = 0; i < n; ++i)
+        {
+            if(leftChild[i] != -1 and ++indegree[leftChild[i]] > 1)
+                return false;
+            if(rightChild[i] != -1 and ++indegree[rightChild[i]] > 1)
+                return false;
+            
+            if(leftChild[i] != -1)
+            {
+                if(dsu.findParent(i) == dsu.findParent(leftChild[i]))
+                   return false;
+                else
+                   dsu.unionBySize(i, leftChild[i]);
+            }
+            
+            if(rightChild[i] != -1)
+            {
+                if(dsu.findParent(i) == dsu.findParent(rightChild[i]))
+                   return false;
+                else
+                    dsu.unionBySize(i, rightChild[i]);
+            }
+        }
+        
+        int cnt = 0;
+        
+        for(int i = 0; i < n; ++i)
+        {
+            if(dsu.findParent(i) == i)
+                ++cnt;
+        }
+        
+        return cnt == 1;
+    }
+};
