a468e9a9541463523a9ae1ce42b6e197155c001e
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-11-10 08:30:12 +0530

    Time: 478 ms (39.09%), Space: 192.1 MB (5.15%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcapital-one-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1743-restore-the-array-from-adjacent-pairs/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcapital-one-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1743-restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        
+        unordered_map<int, vector<int>> adj;
+        unordered_map<int,int> mp;
+        
+        int sv = -1;
+        
+        for(auto& itr : adjacentPairs)
+        {
+            adj[itr[0]].push_back(itr[1]);
+            adj[itr[1]].push_back(itr[0]);
+            
+            ++mp[itr[0]];
+            ++mp[itr[1]];
+        }
+        
+        for(auto& [f, s] : mp)
+        {
+            if(s == 1)
+                sv = f;
+        }
+        
+        vector<int> ans;
+        
+        unordered_set<int> visited;
+        
+        function<void(int)> dfs = [&](int sv)
+        {
+            ans.push_back(sv);
+            visited.insert(sv);
+            
+            for(auto& itr : adj[sv])
+            {
+                if(!visited.count(itr))
+                    dfs(itr);
+            }
+        };
+        
+        cout<<sv<<endl;
+        
+        dfs(sv);
+        
+        return ans;
+        
+    }
+};
