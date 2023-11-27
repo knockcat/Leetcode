7b8f250d221c6eb743274251b6c501bb5f783da4
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-11-27 13:53:01 +0530

    Time: 145 ms (85.55%), Space: 66.2 MB (95.95%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1727-largest-submatrix-with-rearrangements/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1727-largest-submatrix-with-rearrangements.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1727-largest-submatrix-with-rearrangements/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1727-largest-submatrix-with-rearrangements.cpp
@@ -2,27 +2,33 @@ class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
         
-       int i, j, ans = 0, n = matrix.size(), m = matrix[0].size();
+        int n = matrix.size();
+        int m = matrix[0].size();
         
-        for(i = 0; i < m; i++)
+        for(int i = 1; i < n; ++i)
         {
-            for(j = 1; j < n; j++)
+            for(int j = 0; j < m; ++j)
             {
-                if(matrix[j][i] == 1)
-                    matrix[j][i] = matrix[j-1][i] + matrix[j][i];
+                if(matrix[i][j] == 1)
+                    matrix[i][j] += matrix[i-1][j];
                 else
-                    matrix[j][i] = 0;
+                    matrix[i][j] = 0;
             }
         }
-
-        for(i = 0; i < n; i++)
+        
+        int ans = 0;
+        
+        for(int i = 0; i < n; ++i)
         {
-            sort(matrix[i].begin(), matrix[i].end(), greater<int>());
-
-            for(j = 0; j < m; j++)
-                ans = max(ans, matrix[i][j] * (j + 1));
+            sort(matrix[i].begin(), matrix[i].end());
+            
+            for(int j = 0; j < m; ++j)
+            {
+                ans = max(ans, matrix[i][j] * (m-j));
+            }
         }
         
         return ans;
+        
     }
 };
