fc94e317a8267bb217a94721a6c8a70161e2b8b3
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-12-07 09:01:11 +0530

    Time: 23 ms (91.89%), Space: 15.4 MB (37.80%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        
+        int n = num.size();
+        
+        for(int i = n-1; i >= 0; --i)
+        {
+            if((num[i] - '0') & 1)
+                return num.substr(0, i+1);
+        }
+        
+        return "";
+        
+    }
+};
