9d00210669032aaf39b4bbbce181e178f0f2cc49
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-01-26 21:24:53 +0530

    Time: 862 ms (5.01%), Space: 540.5 MB (5.01%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,15 +1,58 @@
 class Solution {
 public:
-     int maxLength(vector<string>& arr, string str = "", int index = 0) {
-	    
-        unordered_set<char>s(str.begin(), str.end());
-        if (s.size() != ((int)str.length())) 
-            return 0;
+    
+    void helper(int idx, int n, string str, set<char> st, int& maxLength, vector<string>& arr)
+    {
+        if(idx == n)
+        {
+            maxLength = max(maxLength, (int)str.size());
+            return;
+        }
         
-        int ret = str.length();
-        for (int i = index; i < arr.size(); i++)
-            ret = max(ret, maxLength(arr, str+arr[i], i+1));
+        helper(idx+1, n, str, st, maxLength, arr);
+        
+        bool canTake = true;
+        
+        set<char> curr;
+        
+        for(auto& ch : arr[idx])
+        {
+            if(curr.count(ch))
+                return;
+            else
+                curr.insert(ch);
+        }
+        
+        if(canTake)
+        {
+            for(auto& ch : curr)
+            {
+                if(st.count(ch))
+                    return;
+            }
+        }
+        
+      
+        for(auto& ch : curr)
+            st.insert(ch);
 
-        return ret;
+        helper(idx+1, n, str + arr[idx], st, maxLength, arr);
+
+        for(auto& ch : curr)
+            st.erase(ch);
+        
+    }
+    
+    int maxLength(vector<string>& arr) {
+        
+        int n = arr.size();
+        
+        set<char> st;
+        string str;
+        int maxLength = 0;
+        
+        helper(0, n, str, st, maxLength, arr);
+        
+        return maxLength;
     }
 };
