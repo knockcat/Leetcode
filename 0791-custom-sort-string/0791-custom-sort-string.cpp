a689ba380b898c723fbcf8ccdd33b9ac5fee6170
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-03-11 11:36:52 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (17.36%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -3,28 +3,18 @@ public:
     string customSortString(string order, string s) {
         
         map<char, int> mp;
-        
-        int id = 0;
-        
+ 
         for(int i = 0; i < order.size(); ++i)
         {
             if(mp.find(order[i]) == mp.end())
-                mp[order[i]] = id++;
+                mp[order[i]] = i;
         }
         
-        vector<pair<char, int>> vp;
+        sort(s.begin(), s.end(),[&](const auto& a, const auto& b){
+            return mp[a] < mp[b];
+        });
         
-        for(auto&ch : s)
-            vp.push_back({mp[ch], ch});
-        
-        sort(vp.begin(), vp.end());
-        
-        string ans;
-        
-        for(auto& ch : vp)
-            ans += ch.second;
-        
-        return ans;
+        return s;
         
     }
 };
