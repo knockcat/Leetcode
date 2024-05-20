026fcf567787c76aed4238088327e57770a11411
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-05-20 09:38:37 +0530

    Time: 189 ms (64.41%), Space: 125.5 MB (89.41%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -2,25 +2,30 @@ class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
         
-    long long sum = 0;
-    int smallest_gain = 2 * k + 1;
-    int smallest_lost = 2 * k + 1;
-    int gain_count = 0;
-    for (int num : nums) {
-      int gain = (num ^ k) - num;
-      if (gain > 0) {
-        smallest_gain = min(smallest_gain, gain);
-        sum += num + gain;
-        ++gain_count;
-      } else {
-        smallest_lost = min(smallest_lost, -gain);
-        sum += num;
-      }
-    }
-    if (gain_count % 2 == 1) {
-      sum -= min(smallest_gain, smallest_lost);
-    }
-    return sum;
+        int mini = 1e9;
+        long long cnt = 0, ans = 0;
         
+        for(auto& ele : nums)
+        {
+            int xorVal = ele ^ k;
+            
+            if(xorVal > ele)
+            {
+                ++cnt;
+                ans += xorVal;
+            }
+            else
+            {
+                ans += ele;
+            }
+            
+            mini = min(mini, abs(xorVal - ele));
+        }
+        
+        if(cnt & 1)
+        {
+            return ans - mini;
+        }
+        return ans;
     }
 };
