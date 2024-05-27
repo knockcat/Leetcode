7871837ca459ef1c2efdd1fb3c8a0d99af543fcc
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-05-27 09:31:35 +0530

    Time: 1184 ms (29.43%), Space: 399.1 MB (32.97%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,33 +1,41 @@
 class Solution {
-private:
-    static const int MOD = 1000000000 + 7;
-
 public:
-    int checkRecord(int n) {
-        vector<vector<int>> prevDP(2, vector<int>(3, 1));
-
-        for (int i = 1; i <= n; i++) {
-            vector<vector<int>> newDP(2, vector<int>(3, 0));
-            for (int a = 0; a < 2; a++) {
-                for (int l = 0; l < 3; l++) {
-                    // Pick P
-                    newDP[a][l] += prevDP[a][2];
-                    newDP[a][l] %= MOD;
-                    if (a > 0) {
-                        // Pick A
-                        newDP[a][l] += prevDP[a - 1][2];
-                        newDP[a][l] %= MOD;
-                    }
-                    if (l > 0) {
-                        // Pick L
-                        newDP[a][l] += prevDP[a][l - 1];
-                        newDP[a][l] %= MOD;
-                    }
-                }
-            }
-            prevDP = newDP;
+    
+    const int mod = 1e9+7;
+    
+    int helper(int n, int absent, int late, vector<vector<vector<int>>>& dp)
+    {
+        if(n == 0)
+            return 1;
+        
+        if(dp[n][absent][late] != -1)
+            return dp[n][absent][late];
+        
+        int total = 0;
+        
+        total += helper(n-1, absent, 2, dp);
+        total %= mod;
+        
+        if(absent > 0)
+        {
+            total += helper(n-1, absent-1, 2, dp);
+            total %= mod;
         }
-
-        return prevDP[1][2];
+        
+        if(late > 0)
+        {
+            total += helper(n-1, absent, late-1, dp);
+            total %= mod;
+        }
+        
+        return dp[n][absent][late] = total;
+    }
+    
+    int checkRecord(int n) {
+        
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, -1)));
+        
+        return helper(n, 1, 2, dp);
+        
     }
 };
