6d88fbbd24d38a665b962afed3a7e3b0d552e9d0
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-06-14 08:19:31 +0530

    Time: 119 ms (62.01%), Space: 69.1 MB (50.39%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -6,17 +6,15 @@ public:
         
         sort(nums.begin(), nums.end());
         
-        int curr = nums[0], ans = 0;
+        int ans = 0;
         
         for(int i = 1; i < n; ++i)
         {
-            if(nums[i] <= curr)
+            if(nums[i-1] >= nums[i])
             {
-                int val = abs(nums[i] - curr) + 1;
-                ans += val;
-                nums[i] += val;
+                ans += (nums[i-1] - nums[i] + 1);
+                nums[i] = nums[i-1] + 1;
             }
-            curr = nums[i];
         }
         
         return ans;
