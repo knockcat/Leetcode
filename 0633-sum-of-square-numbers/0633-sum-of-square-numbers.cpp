f6861ab1f6fafe339233f210e46dcbdcf0d84c95
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-06-17 21:02:20 +0530

    Time: 49 ms (6.20%), Space: 7 MB (91.95%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -2,22 +2,39 @@ using ll = long long;
 
 class Solution {
 public:
-    bool judgeSquareSum(int c) {
+    
+    bool isPerfectSquare(int num)
+    {
+        if(num == 0 or num == 1)
+            return true;
         
-        ll low = 0, high = sqrt(c);
+        ll low = 0, high = num;
         
         while(low <= high)
         {
-            ll mid = low * low + high * high;
+            ll mid = (low + high) / 2;
             
-            if(mid < c)
-                ++low;
-            else if(mid > c)
-                --high;
+            if(mid * mid == num)
+                return true;
+            else if(mid * mid < num)
+                low = mid+1;
             else
+                high = mid-1;
+                
+        }
+        return false;
+    }
+    
+    bool judgeSquareSum(int c) {
+        
+        for(ll i = 0; i * i <= c; ++i)
+        {
+            int a = i * i;
+            if(isPerfectSquare(c - a))
                 return true;
         }
         
         return false;
+        
     }
 };
