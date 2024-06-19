b6918299d146980441b3de7e1c124ca356b4faa6
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-06-19 08:38:34 +0530

    Time: 143 ms (5.04%), Space: 68.9 MB (65.37%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,53 +1,45 @@
+using ll = long long;
+
 class Solution {
 public:
+    
+    int minimumNumberOfDays(int m, int k, int mid, vector<int>& bloomDay)
+    {
+        ll bouquet = 0;
+        ll currSum = 0;
+        
+        for(auto& day : bloomDay)
+        {
+            currSum = (day <= mid ? currSum+1 : 0);
+            bouquet += (currSum / k);
+            currSum %= k;
+        }
+        
+        return bouquet >= m;
+    }
+    
     int minDays(vector<int>& bloomDay, int m, int k) {
-        
-        int n = bloomDay.size();
-        
-        if((long long)k * m > n)
-            return -1;
         
         int low = *min_element(bloomDay.begin(), bloomDay.end());
         int high = *max_element(bloomDay.begin(), bloomDay.end());
         
-        auto canBloom = [&](int mid)
-        {
-            int bloom = 0;
-            int cnt = 0;
-            for(int i = 0; i < n; ++i)
-            {
-                if(bloomDay[i] <= mid)
-                {
-                    ++cnt;
-                }
-                else
-                {
-                    bloom += (cnt / k);
-                    cnt = 0;
-                }
-            }
-            
-            bloom += (cnt / k);
-            
-            return bloom;
-        };
-        
-        int ans = -1;
+        int ans = INT_MAX;
         
         while(low <= high)
         {
-            int mid = low + (high - low) / 2;
+            int mid = (low + high) / 2;
             
-            if(canBloom(mid) >= m)
+            if(minimumNumberOfDays(m, k, mid, bloomDay))
             {
                 ans = mid;
                 high = mid-1;
             }
             else
+            {
                 low = mid+1;
+            }
         }
-                                
-        return ans;                 
         
+        return (ans == INT_MAX ? -1 : ans);
     }
 };
