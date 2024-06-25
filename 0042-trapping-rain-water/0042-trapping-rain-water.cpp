01771bf372ee885a4c39321005d750ff2633fa9a
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-06-25 08:40:49 +0530

    Time: 0 ms (100.00%), Space: 23.2 MB (28.72%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,23 +4,24 @@ public:
         
         int n = height.size();
         
-        int leftMax = 0, rightMax = 0;
+        vector<int> leftMax(n, 0), rightMax(n, 0);
         
-        int ans = 0, i = 0, j = n-1;
+        int ans = 0;
         
-        while(i < j)
+        for(int i = 0; i < n; ++i)
         {
-            leftMax = max(leftMax, height[i]);
-            rightMax = max(rightMax, height[j]);
-            
-            if(height[i] <= height[j])
-            {
-                ans += min(leftMax, rightMax) - height[i++];
-            }
-            else
-            {
-                ans += min(leftMax, rightMax) - height[j--];
-            }
+            leftMax[i] = (i == 0 ? height[i] : max(height[i], leftMax[i-1]));
+        }
+
+        for(int i = n-1; i >= 0; --i)
+        {
+            rightMax[i] = (i == n-1 ? height[i] : max(height[i], rightMax[i+1]));
+        }
+        
+        for(int i = 0; i < n; ++i)
+        {
+            int canHold = min(leftMax[i], rightMax[i]);
+            ans += (canHold - height[i] > 0 ? canHold - height[i] : 0);
         }
         
         return ans;
