f819b816547b99bf111470b1d0d6d0f0b75ebb71
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-07-06 19:31:10 +0530

    Time: 2 ms (54.14%), Space: 7 MB (71.18%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -2,28 +2,10 @@ class Solution {
 public:
     int passThePillow(int n, int time) {
         
-        int dir = 1, pos = 1;
+        int fullChunks = time / (n-1);
+        int extTime = time % (n-1);
         
-        while(time--)
-        {
-            if(dir)
-            {
-                ++pos;
-                if(pos == n)
-                {
-                    dir = 0;
-                }
-            }
-            else
-            {
-                --pos;
-                if(pos == 1)
-                {
-                    dir = 1;
-                }
-            }
-        }
+        return (fullChunks & 1 ? n - extTime : extTime + 1);
         
-        return pos;
     }
 };
