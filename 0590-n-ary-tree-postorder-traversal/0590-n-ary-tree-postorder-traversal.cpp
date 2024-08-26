627eef7c7f9de705355ece74b8b06575fa3d23af
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2024-08-26 20:59:51 +0530

    Time: 18 ms (22.62%), Space: 15.3 MB (49.47%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    void postorder(Node* root, vector<int>& ans)
+    {
+        if(root)
+        {
+            for(auto& chlNode : root->children)
+            {
+                postorder(chlNode, ans);
+                ans.push_back(chlNode->val);
+            }
+        }
+    }
+    
+    vector<int> postorder(Node* root) {
+        
+        vector<int> ans;
+        
+        postorder(root, ans);
+        if(root)
+            ans.push_back(root->val);
+        
+        return ans;
+        
+    }
+};
